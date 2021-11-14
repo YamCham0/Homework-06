4329991cc026223c579d7b023ef0b1c5fcb71d9c
Yamil Chamo <yajochakha@gmail.com>
2021-11-13 18:17:59 -0600

    read me and final commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Homework-06
- 
+# Weather Dashboard
+
+# For the Grader
+* I forgot to clone the repo and did it locally.  
+* In the image folder, you can find a screenshot of the local repository with date and time of edit and creation.
+* Comming back to this one later to apply some styles.
+
+## When:
+
+* User input and submit is presented with current weather data and a 5 day forecast.
+
+
+
+
+[Link Here!](https://yamcham0.github.io/Homework-06/)
+
+![Screenshot of the Daily Planner main page.](./assets/images/weatherDasdh.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -165,33 +165,3 @@ function superBigBox(currentHumidity, currentTemperature, currentUVI, curreentWi
     node.appendChild(humidity);
     document.getElementById("bigBox").appendChild(node);
 };
-
-// append EE to Div
-
-
-// append node to the container
-
-
-
-
-
-
-// Card
-
-{/* <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div> */}
-
-
-
-
-
-
-// Form
-
-      
